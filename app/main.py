5b9ef5b57e75ed1feec5ea14d4f32047a670a802
Kushagra <writekushagra12@gmail.com>
2025-07-10 01:20:31 +0530

    improved

--- app/main.py
+++ app/main.py
@@ -240,7 +240,7 @@ st.markdown("""
 
     </style>
     <div class="top-banner">
-        🪙<span style='color:#FFFFFF; font-weight:900; font-size:44px;'>StockMatrix</span>
+        🪙<span style='color:#FFFFFF !important; font-weight:900; font-size:44px; text-shadow: 0 2px 6px rgba(0,0,0,0.5);'>StockMatrix</span>
     </div>
 """, unsafe_allow_html=True)
 
